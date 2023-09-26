e8bd1d968c6b80daddab3d4de24431f7d209d0ed
Aapo (f021b) Rista <aapris@gmail.com>
2023-09-26 14:54:54 +0300

    Remove `extra` from model for now

--- fvhiot/models/requests.py
+++ fvhiot/models/requests.py
@@ -22,12 +22,7 @@ class RequestModel(BaseModel):
 
 from typing import Any, Dict
 
-from pydantic import (
-    BaseModel,
-    StrictStr,
-    StrictBytes,
-    root_validator,
-)
+from pydantic import model_validator, BaseModel, StrictStr, StrictBytes
 
 
 class RequestData(BaseModel):
@@ -37,17 +32,18 @@ class RequestData(BaseModel):
     headers: dict
     files: dict
     body: StrictBytes
-    extra: Dict[str, Any]
-
-    @root_validator(pre=True)
-    def build_extra(cls, values: Dict[str, Any]) -> Dict[str, Any]:
-        all_required_field_names = {field.alias for field in cls.__fields__.values() if field.alias != "extra"}
-        extra: Dict[str, Any] = {}
-        for field_name in list(values):
-            if field_name not in all_required_field_names:
-                extra[field_name] = values.pop(field_name)
-        values["extra"] = extra
-        return values
+    # extra: Dict[str, Any]
+    #
+    # @model_validator(mode="before")
+    # @classmethod
+    # def build_extra(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+    #     all_required_field_names = {field.alias for field in cls.model_fields.values() if field.alias != "extra"}
+    #     extra: Dict[str, Any] = {}
+    #     for field_name in list(values):
+    #         if field_name not in all_required_field_names:
+    #             extra[field_name] = values.pop(field_name)
+    #     values["extra"] = extra
+    #     return values
 
 
 class RequestModel(BaseModel):
@@ -57,14 +53,16 @@ class RequestModel(BaseModel):
     scheme: StrictStr
     remote_addr: StrictStr
     request: RequestData
-    extra: Dict[str, Any]
-
-    @root_validator(pre=True)
-    def build_extra(cls, values: Dict[str, Any]) -> Dict[str, Any]:
-        all_required_field_names = {field.alias for field in cls.__fields__.values() if field.alias != "extra"}
-        extra: Dict[str, Any] = {}
-        for field_name in list(values):
-            if field_name not in all_required_field_names:
-                extra[field_name] = values.pop(field_name)
-        values["extra"] = extra
-        return values
+    # extra: Dict[str, Any]
+    #
+    # @model_validator(mode="before")
+    # @classmethod
+    # def build_extra(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+    #     all_required_field_names = {field.alias for field in cls.model_fields.values() if field.alias != "extra"}
+    #     extra: Dict[str, Any] = {}
+    #     for field_name in list(values):
+    #         if field_name not in all_required_field_names:
+    #             extra[field_name] = values.pop(field_name)
+    #     values["extra"] = extra
+    #     print("MOIOIIII", values)
+    #     return values
